af7d2775c1780facb7891d82d5c05ce6d770173c
Rania Zarka <rani0401@edu.easj.dk>
2020-08-25 13:13:02 +0200

    changes

--- MyFirstGitHubClassRoomConsoleApp/Program.cs
+++ MyFirstGitHubClassRoomConsoleApp/Program.cs
@@ -12,6 +12,11 @@ namespace MyFirstGitHubClassRoomConsoleApp
         {
             //Opgave tilføj en Console.writeline med noget tekst og commit + push
             //se herefter på dit GitHub Classroom repo om ændringen er kommet op 
+
+            int number;
+            string name;
+
+
         }
     }
 }
